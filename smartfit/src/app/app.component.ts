8b333549b0b09b7fb4fb7427f499b071bb892605
Stanley Dias Paulo <stanleydevbr@gmail.com>
2024-02-15 15:12:40 -0300

    feat: add componente de formulario.

--- smartfit/src/app/app.component.ts
+++ smartfit/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsComponent]
 })
 export class AppComponent {
   title = 'smartfit';
---
+++ smartfit/src/app/components/forms/forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devbr-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+}

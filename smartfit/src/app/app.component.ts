d247f8d552bd60a0d8106417e728001ab330e5fa
Stanley Dias Paulo <stanleydevbr@gmail.com>
2024-02-20 07:38:13 -0300

    feat: atualização

--- smartfit/src/app/app.component.ts
+++ smartfit/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { FormsComponent } from "./components/forms/forms.component";
+import { UnidadesComponent } from "./components/unidades/unidades.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsComponent, UnidadesComponent]
 })
 export class AppComponent {
   title = 'smartfit';
---
+++ smartfit/src/app/components/unidades/unidades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devbr-unidades',
+  standalone: true,
+  imports: [],
+  templateUrl: './unidades.component.html',
+  styleUrl: './unidades.component.scss'
+})
+export class UnidadesComponent {
+
+}
